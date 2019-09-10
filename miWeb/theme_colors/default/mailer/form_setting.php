0ad09d72bde3c75451c354781401caea89b9d459
Guillermo Risoli <guille.risoli@gmail.com>
2019-09-10 14:09:18 -0700

    new fonts

--- miWeb/theme_colors/default/mailer/form_setting.php
+++ miWeb/theme_colors/default/mailer/form_setting.php
@@ -1,7 +1,7 @@
 <?php
 	/*Form settings*/
-	$subj = "New message from the site 'vCard & Resume Template'"; //letter subject
-	$to = 'beshleyua@gmail.com'; // Enter Your E-mail
+	$subj = "New message from the personal page"; //letter subject
+	$to = 'guille.risoli@gmail.com'; // Enter Your E-mail
 	$from = 'admin@you-site-name.com'; // Admin e-mail
 	$fromName = 'Your Company Name'; // Your company name
 	$charset = 'UTF-8';
