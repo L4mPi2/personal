397aa7a16ba7345422719e4aba7a076894ac8c67
Guillermo Risoli <guille.risoli@gmail.com>
2019-09-17 20:02:22 -0700

    mailer

--- miWeb/theme_colors/default/mailer/form_setting.php
+++ miWeb/theme_colors/default/mailer/form_setting.php
@@ -1,8 +1,8 @@
 <?php
 	/*Form settings*/
-	$subj = "New message from the personal page"; //letter subject
+	$subj = "Hi"; //letter subject
 	$to = 'guille.risoli@gmail.com'; // Enter Your E-mail
 	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'Your Company Name'; // Your company name
+	$fromName = 'Guillermo Risoli'; // Your company name
 	$charset = 'UTF-8';
 ?>
