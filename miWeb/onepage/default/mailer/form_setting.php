ee3326ebf7e124c6997f0b93f67248d68707b3dd
Guillermo Risoli <guille.risoli@gmail.com>
2019-09-17 23:23:10 -0700

    remove stuff

--- miWeb/onepage/default/mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "Hi"; //letter subject
-	$to = 'guille.risoli@gmail.com'; // Enter Your E-mail
-	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'Guillermo Risoli'; // Your company name
-	$charset = 'UTF-8';
-?>
